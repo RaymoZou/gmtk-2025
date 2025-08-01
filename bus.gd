a68d8437e12d6293467db58de3d4abe137dee3c2
RaymoZou <raymond.1zou@gmail.com>
2025-07-31 23:21:29 -0700

    add bus scene and TOOOs

---
+++ bus.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+# TODO: make the bus follow a predetermined route
+
+func _init() -> void:
+	print("i am a bus")
+	
+# TODO:
+func move():
+	return
+	
+func _process(delta: float) -> void:
+	move()
