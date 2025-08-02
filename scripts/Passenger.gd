d0095c5fa3015236a5b9aea4bd25b6148c7ef895
Kevin Zou <kevin.zp.zou@gmail.com>
2025-08-02 14:51:55 -0700

    wip

--- scripts/Passenger.gd
+++ scripts/Passenger.gd
@@ -2,10 +2,13 @@ extends Node
 
 class_name Passenger
 
-var satisfaction: int
+var satisfaction: int = 0
+var origin_station: Station
 var target_station: Station
 const FARE : int = 5
 @onready var area3d : Area3D = %Area3D
+
+signal left_bus(fare: int, satisfaction: int)
 
 # TODO: create a station that spawns passengers - they are not meant to
 #     	instatiated directly
@@ -18,8 +21,9 @@ func _ready() -> void:
 func _on_area_entered(area3d : Area3D):
 	SignalBus.passenger_picked_up.emit(self)
 
-
 func leave_bus() -> void:
 	print("Passenger leaving bus")
+	left_bus.emit(3, satisfaction)
 	# TODO: based on individual satisifaction, alter the game state satisfaction
+
 	# TODO: pay fare -> add money to game state
