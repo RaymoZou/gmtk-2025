e2868d25e1e096b6d5798a79fea1338644bf692d
Kevin Zou <kevin.zp.zou@gmail.com>
2025-08-02 23:16:03 -0700

    finally (#8)

--- scripts/Station.gd
+++ scripts/Station.gd
@@ -14,16 +14,34 @@ func _init() -> void:
 	# TODO: make a random name
 	
 func _ready() -> void:
+	# Regsiter station with GameManager
+	GameManager.stations.append(self)
+	
 	# TODO: Maybe change this to use the station node position
 	var collision_node: CollisionShape3D = self.get_node("Area3D/CollisionShape3D")
 	var size = collision_node.shape.get_debug_mesh().get_aabb().size
 	spawn_point = (Vector3(-1, 0, 1) * size / 2) + Vector3(0, 0, 2)
+	
+	# Set up area collision detection
+	var area_node: Area3D = self.get_node("Area3D")
+	area_node.area_entered.connect(_on_area_entered)
 
-func _on_passenger_spawn_timer_timeout() -> void:
-	print("SPAWN PASSENGERS")
-	var num_passengers = randi_range(1, 3)
+func _on_area_entered(area: Area3D) -> void:
+	if area.name == "BusArea":
+		# Drop off passengers first
+		%Bus.unload_passengers(self)
+		# Then load passengers
+		if len(passengers):
+			%Bus.load_passengers(passengers)
+			passengers.clear()
+		
+func spawn_passengers():
+	var num_passengers = randf_range(1, 1)
+	var all_other_stations = GameManager.stations.filter(func(s): return s != self)
 	for i in num_passengers:
-		var new_passenger = passenger_scene.instantiate()
+		var new_passenger: Passenger = passenger_scene.instantiate()
+		new_passenger.origin_station = self
+		new_passenger.target_station = all_other_stations.pick_random()
 		new_passenger.position = spawn_point + len(passengers) * Vector3(0, 0, 3)
 		add_child(new_passenger)
 		passengers.append(new_passenger)
