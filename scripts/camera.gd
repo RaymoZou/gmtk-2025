398aa68f768617b0451f9ef8366c149128fb7b19
RaymoZou <raymond.1zou@gmail.com>
2025-08-07 23:21:21 -0700

    add: zoom in/out limit

--- scripts/camera.gd
+++ scripts/camera.gd
@@ -1,13 +1,15 @@
 extends Camera3D
 
+const MAX_ZOOM_IN : int = 10
+const MAX_ZOOM_OUT : int = 200
 const ZOOM_AMOUNT : int = 10
 const PAN : float = 40
 
 func _process(delta : float) -> void:
-	if Input.is_action_just_released("scroll_up"):
+	if Input.is_action_just_released("scroll_up") and size > MAX_ZOOM_IN:
 		size -= ZOOM_AMOUNT
 		
-	if Input.is_action_just_released("scroll_down"):
+	if Input.is_action_just_released("scroll_down") and size <= MAX_ZOOM_OUT:
 		size += ZOOM_AMOUNT
 		
 	if Input.is_action_pressed("ui_right"):
