e94d7659687caa9c682b9d96cce020e6fa679625
RaymoZou <raymond.1zou@gmail.com>
2025-08-16 13:45:58 -0700

    add: bus upgrade sfx

--- scripts/audio_manager.gd
+++ scripts/audio_manager.gd
@@ -1,8 +1,9 @@
 extends AudioStreamPlayer
 
-# func _ready() -> void:
-# 	SignalBus.passenger_dropped_off.connect(_on_passenger_dropped_off)
-	
-# func _on_passenger_dropped_off(_money: int, _satisfaction: int):
-# 	# print("sfx: passenger dropped off")
-# 	play()
+func _ready() -> void:
+	# for now, play a sound effect whenever the bus state changes
+	# might need specific signals to be emitted in the future
+	SignalBus.bus_updated.connect(_on_bus_updated)
+
+func _on_bus_updated(_bus: Bus):
+	play()
